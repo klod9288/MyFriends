25e30ee8ad85cce958ff4aa721e2acc369fd8758
srongklod <klod9288@gmail.com>
2018-03-16 11:00:35 +0700

    Design UI Register.

---
+++ app/src/main/java/com/example/srongklod/myfriends/Register.java
@@ -0,0 +1,11 @@
+package com.example.srongklod.myfriends;
+
+/**
+ * Created by srongklod on 16/3/2018 AD.
+ */
+
+public class Register {
+
+
+
+}
---
+++ app/src/main/java/com/example/srongklod/myfriends/fragment/Mainfragment.java
@@ -0,0 +1,8 @@
+package com.example.srongklod.myfriends.fragment;
+
+/**
+ * Created by srongklod on 16/3/2018 AD.
+ */
+
+public class Mainfragment {
+}
